27aea597dcb2e94d0160515d3d51ec8de5fad660
Justin Paul Silva <jpsilva@Justins-MacBook-Pro.local>
2016-12-24 23:27:03 -0500

    Added genome building. Added product descriptions.

---
+++ genomefile.js
@@ -0,0 +1,50 @@
+const browserSync = require('browser-sync'),
+      cpx = require('cpx'),
+      genome = require('genome'),
+      slm = require('slm'),
+      stylus = require('stylus');
+
+genome.tasks = {
+  * copyImages() {
+    return cpx.copySync('src/images/*.{jpeg,jpg,gif,png,svg}', 'images');
+  },
+
+  * slmToHtml() {
+    let file = yield 'src/index.slm'.read();
+    return 'index.html'.write(slm.render(file));
+  },
+
+  * stylusToCss() {
+    let file = yield 'src/styles/site.styl'.read();
+    return 'styles/site.css'.write(stylus(file).render());
+  },
+
+  * watch() {
+    yield genome.spawn(['copyImages', 'slmToHtml', 'stylusToCss']);
+
+    let server = serve();
+
+    'src/images/*.{jpeg,jpg,gif,png,svg}'.onChange('copyImages');
+    'src/index.slm'.onChange('slmToHtml');
+    'src/styles/**/*.styl'.onChange('stylusToCss');
+    '**/*.{css,html}'.onChange(server.reload);
+  }
+};
+
+function serve() {
+  let server = browserSync.create();
+
+  server.init({
+    server: {
+      baseDir: '.'
+    },
+    open: false,
+    reloadOnRestart: true,
+    notify: false,
+    ghostMode: false
+  });
+
+  return server;
+}
+
+genome.run('watch');
