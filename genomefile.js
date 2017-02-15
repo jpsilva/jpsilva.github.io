8ff82eeabfcc8fcee7630e9d6490b89319ab46cd
Justin Paul Silva <justin@jpsilva.com>
2017-02-15 18:21:09 -0500

    Added incomplete superscribe page. Added link to superscribe on amazon.

--- genomefile.js
+++ genomefile.js
@@ -14,19 +14,25 @@ genome.tasks = {
     return 'webfonts/index.html'.write(slm.render(file));
   },
 
-  * stylusToCss() {
-    let file = yield 'src/styles/site.styl'.read();
-    return 'styles/site.css'.write(stylus(file).render());
+  * superscribeSlmToHtml() {
+    let file = yield 'src/superscribe/index.slm'.read();
+    return 'superscribe/index.html'.write(slm.render(file));
   },
 
+  // * stylusToCss() {
+  //   let file = yield 'src/styles/site.styl'.read();
+  //   return 'styles/site.css'.write(stylus(file).render());
+  // },
+
   * watch() {
-    yield genome.spawn(['slmToHtml', 'fontSlmToHtml', 'stylusToCss']);
+    yield genome.spawn(['slmToHtml', 'fontSlmToHtml', 'superscribeSlmToHtml']);
 
     let server = serve();
 
     'src/index.slm'.onChange('slmToHtml');
+    'src/superscribe/index.slm'.onChange('superscribeSlmToHtml');
     'src/webfonts/index.slm'.onChange('fontSlmToHtml');
-    'src/styles/**/*.styl'.onChange('stylusToCss');
+    // 'src/styles/**/*.styl'.onChange('stylusToCss');
     '**/*.{css,html,jpeg,jpg,gif,png,svg}'.onChange(server.reload);
   }
 };
